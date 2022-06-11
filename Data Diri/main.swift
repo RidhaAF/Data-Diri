13fa91e299b86766f6435e26d7c4362f165fe8d3
ridhaaf <ridhaaf@gmail.com>
2022-06-11 14:47:32 +0700

    Update main.swift

--- Data Diri/main.swift
+++ Data Diri/main.swift
@@ -7,5 +7,44 @@
 
 import Foundation
 
-print("Hello, World!")
+//// form biodata
+//print("Selamat Datang di Dicoding Academy")
+//
+//let firstName = "Ridha Ahmad", lastName = "Firdaus"
+//let fullName = firstName + " " + lastName
+//let address = "Bandung"
+//let job = "iOS Developer"
+//let age = 21
+//
+//print("----------------------------------")
+//
+//// print to terminal
+//print("Apakah kamu tahu \(fullName)?")
+//print("\(fullName) adalah seorang \(job).")
+//print("Saat ini dia berumur \(age) dan bertempat tinggal di \(address).")
+//
+//print("----------------------------------")
+
+// form biodata
+print("Selamat Datang di Dicoding Academy")
+print("----------------------------------")
+
+// input value from user
+print("Masukkan nama depan Anda:"); let firstName = readLine()!
+print("Masukkan nama belakang Anda:"); let lastName = readLine()!
+print("Masukkan umur Anda:"); let age = readLine()!
+print("Masukkan alamat Anda:"); let address = readLine()!
+print("Masukkan pekerjaan Anda:"); let job = readLine()!
+
+// combine first name & last name
+let fullName = firstName + " " + lastName
+
+print("----------------------------------")
+
+// print to terminal
+print("Apakah kamu tahu \(fullName)?")
+print("\(fullName) adalah seorang \(job).")
+print("Saat ini ia berumur \(age) tahun dan bertempat tinggal di \(address).")
+
+print("----------------------------------")
 
